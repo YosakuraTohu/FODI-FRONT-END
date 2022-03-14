519e9a7d19bec9a460645d659c695d0b7145c8e2
sherlock_lopez@outlook.com <sherlock_lopez@outlook.com>
2022-03-15 02:09:41 +0800

    Update

---
+++ book/cookie.js
@@ -0,0 +1,25 @@
+var username = document.cookie.split(";")[0].split("=")[1];
+//JS操作cookies方法!
+//写cookies
+async function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+async function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
+async function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null)
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
